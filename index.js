8ac59a59dd93529da3425c10cda893f2d4621606
Amanda Pollard <Amandappollard@gmail.com>
2025-01-17 00:27:50 +0000

    that was fast

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-app.post("api/fileanalyse", upload.single('upfile'), (req, res) => {
+app.post("/api/fileanalyse", upload.single('upfile'), (req, res) => {
   res.json({
     name: req.file.originalname,
     type: req.file.mimetype,
